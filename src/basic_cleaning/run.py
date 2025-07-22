0de5b66dc520feb76e89ab231126c9595f6358f9
alvxp <77099211+alvxp@users.noreply.github.com>
2025-07-23 01:59:38 +0800

    Second push with updates for data cleaning.

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """
-Performs basic cleaning on the data and saves the results in W&B.
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
 import wandb
-
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -24,30 +24,69 @@ def go(args):
     # YOUR CODE HERE     #
     ######################
 
+    logger.info("Start of run")
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+
+    df = pd.read_parquet(artifact_path)
+
+    logger.info("Cleaning of data")
+    df = df[df['price'].between(args.min_price, args.max_price)]
+
+    logger.info("Exporting of clean data")
+    df.to_csv = ('clean_sample.csv', index=False)
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This step cleans the data.")
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
 
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact", 
+        type=str,
+        help='Fully-qualified name of input artifact for cleaning',
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact",
+        type=str,
+        help='Fully-qualified name of output artifact for cleaned data',
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_type", 
+        type=str,
+        help='Type of output dataset',
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help='Description of output dataset',
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="Minimum price for rental",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float,
+        help='Maximum price for rental',
         required=True
     )
 
